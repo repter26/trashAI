2e8e0872d9f2b5e862f23be3f42df9977787eaa2
Retepai <156745107+repter26@users.noreply.github.com>
2024-09-21 14:15:33 -0700

    redo

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,15 @@
 import { useState, useEffect } from 'react'
-import Camera from './components/Camera.jsx'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import NavBar from "./components/NavBar/NavBar.jsx"
+import OnBoarding from "./components/OnBoarding/OnBoarding.jsx"
 
 function App() {
-  // const [data, setData] = useState([]);
-  // const [loading, setLoading] = useState(true);
-
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     const response = await fetch('http://127.0.0.1:5000/');
-  //     const result = await response.text();
-  //     setData(result);
-  //     setLoading(false);
-  //   };
-
-  //   fetchData();
-  // }, []); 
-  
-  // // Empty array means this runs once when the component mounts
-  // if (loading) {
-  //   return <div>Loading...</div>;
-  // }
-
   return (
     <>
-      <h1>Vite + React</h1>
-      <div className="card">
-          {data}
-      </div>
-      <Camera />
+      <NavBar/>
+      <OnBoarding/>
     </>
   )
 }
