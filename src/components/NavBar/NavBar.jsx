198f39f282ea5e12addf996206dbf688e2c36812
Retepai <156745107+repter26@users.noreply.github.com>
2024-09-21 13:03:09 -0700

    added image and nav

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -6,10 +6,14 @@ function NavBar() {
 
   return (
     <>
-      <div className = "flex flex-row">
-        <img className = "ml-10 mt-5 h-20 w-15 shadow" src = {trashImg}/>
-        <h1 className = "mt-10 ml-5 text-3xl font-bold "> TRASH.AI</h1>
+      <div className = "flex flex-row h-32 bg-opacity-50 shadow">
+        <img className = "my-auto ml-12 h-20 w-15" src = {trashImg}/>
+        <h1 className = "my-auto ml-5 text-3xl font-bold "> TRASH.AI</h1>
+        
+        <a className="my-auto ml-auto mr-20 text-xl font-bold hover:text-green-500 transition-colors duration-300">About</a>
+        <button className="my-auto mr-20 w-12 h-12 bg-black rounded-md text-white font-bold  hover:bg-green-500 transition-colors duration-300"> AI</button>
       </div>
+      
     </>
   )
 }
