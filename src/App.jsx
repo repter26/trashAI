ca4bfde6e1dfe77c9a694076e3f73e0e4109a1e3
Retepai <156745107+repter26@users.noreply.github.com>
2024-09-21 11:04:02 -0700

    tailwind ccs config

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
+      <div className="flex flex-col item-center justify-center ml-100">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
