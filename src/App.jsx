ecc53fb43ef61669c82dd1185b97add88fedd2cf
Pojj <williambsong@gmail.com>
2024-09-22 02:48:00 -0700

    fix background

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,18 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Camera from "./components/Camera/Camera.jsx";
 import NavBar from "./components/NavBar/NavBar.jsx";
 import OnBoarding from "./components/OnBoarding/OnBoarding.jsx";
-import robotImg from "./assets/robotImg.png"; // Ensure this path is correct
 import "./App.css";
 
 function App() {
   return (
     <Router>
-      <div
-        className="h-screen bg-cover bg-center-top"
-        style={{ backgroundImage: `url(${robotImg})` }}
-      >
-        <NavBar />
-        {/* Define Routes */}
-        <Routes>
-          {/* Default route */}
-          <Route path="/" element={<OnBoarding />} />
-          {/* Route for the Camera component */}
-          <Route path="/camera" element={<Camera />} />
-        </Routes>
-      </div>
+      {/* Define Routes */}
+      <Routes>
+        {/* Default route */}
+        <Route path="/" element={<OnBoarding />} />
+        {/* Route for the Camera component */}
+        <Route path="/camera" element={<Camera />} />
+      </Routes>
     </Router>
   );
 }
--- src/components/OnBoarding/OnBoarding.jsx
+++ src/components/OnBoarding/OnBoarding.jsx
@@ -1,10 +1,15 @@
 import { useState } from "react";
 import "./OnBoarding.css";
 import robotImg from "../../assets/robotImg.png"; // Ensure this path is correct
+import NavBar from "../NavBar/NavBar";
 
 function OnBoarding() {
   return (
-    <>
+    <div
+      className="h-screen bg-cover bg-center-top"
+      style={{ backgroundImage: `url(${robotImg})` }}
+    >
+      <NavBar />
       <div className="flex flex-col justify-center items-end ">
         <h1 className="mt-40 mb-10 mr-20 text-6xl text-white font-bold text-right">
           Intelligent Trash Sorting
@@ -14,7 +19,7 @@ function OnBoarding() {
           management by automatically detecting and sorting trash.{" "}
         </p>
       </div>
-    </>
+    </div>
   );
 }
 
